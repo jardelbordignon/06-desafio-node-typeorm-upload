7664c56086544955e6bcb44b35699a2d47545398
Jardel <jardel1101@gmail.com>
2020-08-23 23:33:56 -0300

    Alterções finalizadas

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,18 +1,36 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm'
 
-import Transaction from '../models/Transaction';
+import Transaction from '../models/Transaction'
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+  income: number
+  outcome: number
+  total: number
 }
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transaction = await this.find()
+
+    const { income, outcome } = transaction.reduce((acc, transaction) => {
+      if (transaction.type === 'income')
+        acc.income += Number(transaction.value)
+      
+      if (transaction.type === 'outcome')
+        acc.outcome += Number(transaction.value)
+      
+      return acc
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    })
+
+    const total = income - outcome
+
+    return { income, outcome, total }
   }
 }
 
-export default TransactionsRepository;
+export default TransactionsRepository
